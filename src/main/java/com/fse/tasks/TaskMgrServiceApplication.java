68107880ac4502d010124e179973523e662fb851
Thiagarajan, Balasubramaniam R (C) <balasubramaniam.thiagarajan@csaa.com>
2019-09-26 10:46:35 -0700

    First commit

--- src/main/java/com/fse/tasks/TaskMgrServiceApplication.java
+++ src/main/java/com/fse/tasks/TaskMgrServiceApplication.java
@@ -3,11 +3,18 @@ package com.fse.tasks;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
 @SpringBootApplication
-public class TaskMgrServiceApplication {
+public class TaskMgrServiceApplication  extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(TaskMgrServiceApplication.class, args);
 	}
+	    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(TaskMgrServiceApplication.class);
+    }
 
 }
